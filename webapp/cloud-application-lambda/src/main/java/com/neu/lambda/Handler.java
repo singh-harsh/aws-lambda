de7a090cd5bd1f14d723424a2d773475ee627496
hardik-j <jain.h@husky.neu.edu>
2018-11-10 15:13:12 -0500

    Fixing codedeploy

--- webapp/cloud-application-lambda/src/main/java/com/neu/lambda/Handler.java
+++ webapp/cloud-application-lambda/src/main/java/com/neu/lambda/Handler.java
@@ -7,31 +7,39 @@ import com.amazonaws.services.lambda.runtime.events.SNSEvent;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
 import com.amazonaws.services.simpleemail.model.*;
+import com.neu.db.ResetTokenHandler;
 
 public class Handler implements RequestHandler<SNSEvent, Object> {
+
+    private ResetTokenHandler tokenHandler = new ResetTokenHandler();
 
     public Object handleRequest(SNSEvent snsEvent, Context context) {
         String emailAddress = snsEvent.getRecords().get(0).getSNS().getMessage();
         //Validate for DynamoDB TTL and send an e-mail if authenticated
-        try {
-            AmazonSimpleEmailService client =
-                    AmazonSimpleEmailServiceClientBuilder.standard()
-                            .withRegion(Regions.US_EAST_1).build();
-            SendEmailRequest request = new SendEmailRequest()
-                    .withDestination(
-                            new Destination().withToAddresses(emailAddress))
-                    .withMessage(new Message()
-                            .withBody(new Body()
-                                    .withText(new Content()
-                                            .withCharset("UTF-8").withData("Bunty sends his regards")))
-                            .withSubject(new Content()
-                                    .withCharset("UTF-8").withData("Apna kaam kar na")))
-                    .withSource("bunty@csye6225-fall2018-singhhar.me");
-            client.sendEmail(request);
-        } catch (Exception ex) {
-            System.out.println("The email was not sent. Error message: "
-                    + ex.getMessage());
+        String token = tokenHandler.generateToken(emailAddress);
+        if(token != null) {
+            try {
+                AmazonSimpleEmailService client =
+                        AmazonSimpleEmailServiceClientBuilder.standard()
+                                .withRegion(Regions.US_EAST_1).build();
+                SendEmailRequest request = new SendEmailRequest()
+                        .withDestination(
+                                new Destination().withToAddresses(emailAddress))
+                        .withMessage(new Message()
+                                .withBody(new Body()
+                                        .withText(new Content()
+                                                .withCharset("UTF-8").withData("Bunty sends his regards for " + emailAddress + " and token is : " + token)))
+                                .withSubject(new Content()
+                                        .withCharset("UTF-8").withData("Apna kaam kar na")))
+                        .withSource("bunty@csye6225-fall2018-singhhar.me");
+                client.sendEmail(request);
+            } catch (Exception ex) {
+                System.out.println("The email was not sent. Error message: "
+                        + ex.getMessage());
+            }
+            return emailAddress;
         }
-        return emailAddress;
+        return "The email was not sent!!";
     }
+
 }
