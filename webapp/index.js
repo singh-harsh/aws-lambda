a482cc129f37e5a14aa9e352ba0ec6cbeb9aa8c9
Harsh Singh <singh.har@husky.neu.edu>
2018-11-11 19:31:22 -0500

    mail sending done

--- webapp/index.js
+++ webapp/index.js
@@ -2,21 +2,19 @@ console.log('Loading function');
 
 var aws = require('aws-sdk');
 var ses = new aws.SES({
-  region: 'us-east-1' 
+    region: 'us-east-1'
 });
-exports.handler = async (event, context) => {
-    //console.log('Received event:', JSON.stringify(event, null, 2));
-    const message = event.Records[0].Sns.Message;
-    //rvice object, passing the parameters. Then handle the response in the promise callback.
 
-    // Load the AWS SDK for Node.js
-    
+exports.handler = async(event, context) => {
 
+    const to = event.Records[0].Sns.MessageAttributes.email.Value;
+    const domainName = event.Records[0].Sns.MessageAttributes.domainName.Value;
+    const from = "do-not-reply@" + domainName;
     // Create sendEmail params 
     var params = {
         Destination: { /* required */
             ToAddresses: [
-                message
+                to
                 /* more items */
             ]
         },
@@ -24,33 +22,17 @@ exports.handler = async (event, context) => {
             Body: { /* required */
                 Text: {
                     Charset: "UTF-8",
-                    Data: "Kaam kar apna"
+                    Data: "Click the link below to reset your password:"
                 }
             },
             Subject: {
                 Charset: 'UTF-8',
-                Data: 'Bunty'
+                Data: 'Reset password at MyCloudWebApi'
             }
         },
-        Source: 'bunty@csye6225-fall2018-singhhar.me', /* required */
+        Source: from,
+        /* required */
     };
     console.log('===SENDING EMAIL===');
-    var email = ses.sendEmail(params, function(err, data){
-        console.log("Inside Sending Email")
-    if(err) {
-       console.log(err);
-       context.fail(err);
-    }
-    else {
-        console.log("===EMAIL SENT===");
-        console.log("EMAIL CODE END");
-        console.log('EMAIL: ', email);
-        console.log(data);
-        context.succeed(event);
-        
-    }
-});
-    console.log("Lambda hit")
-   // return message;
+    var sendPromise = await ses.sendEmail(params).promise();
 };
-
