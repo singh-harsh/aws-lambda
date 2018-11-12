d2a0b7476a5e5fc619f1a3884b07ba56d5302e1a
Harsh Singh <singh.har@husky.neu.edu>
2018-11-12 01:13:01 -0500

    lambda send email success

--- webapp/index.js
+++ webapp/index.js
@@ -5,11 +5,23 @@ var ses = new aws.SES({
     region: 'us-east-1'
 });
 
-exports.handler = async(event, context) => {
+exports.handler = async (event, context) => {
 
-    const to = event.Records[0].Sns.MessageAttributes.email.Value;
-    const domainName = event.Records[0].Sns.MessageAttributes.domainName.Value;
-    const from = "do-not-reply@" + domainName;
+    const to = event.Records[0].Sns.Message;
+
+    // Create listIdentities params 
+    var params1 = {
+        IdentityType: "Domain",
+        MaxItems: 10
+    };
+
+    // Create the promise and SES service object
+    var listIDsPromise = await new aws.SES({ apiVersion: '2010-12-01' }).listIdentities(params1).promise();
+    const domainName = listIDsPromise.Identities[0];
+
+    var from = "do-not-reply@";
+    from += domainName;
+
     // Create sendEmail params 
     var params = {
         Destination: { /* required */
@@ -20,9 +32,9 @@ exports.handler = async(event, context) => {
         },
         Message: { /* required */
             Body: { /* required */
-                Text: {
+                HTML: {
                     Charset: "UTF-8",
-                    Data: "Click the link below to reset your password:"
+                    Data: "<p>Click the link below to reset your password:</p> <br> <b>http://mycloudwebapi.com/reset?email=" + to + "&token=4e163b8b-889a-4ce7-a3f7-61041e323c23<b>"
                 }
             },
             Subject: {
@@ -34,5 +46,5 @@ exports.handler = async(event, context) => {
         /* required */
     };
     console.log('===SENDING EMAIL===');
-    var sendPromise = await ses.sendEmail(params).promise();
+    await ses.sendEmail(params).promise();
 };
